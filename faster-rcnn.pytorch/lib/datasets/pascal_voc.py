5c6891b6022be5696f25cec822becf7bc79e3fe4
aaron <aaron.wang.de@gmail.com>
2020-02-21 19:50:15 +0100

    pascal_voc.py

--- faster-rcnn.pytorch/lib/datasets/pascal_voc.py
+++ faster-rcnn.pytorch/lib/datasets/pascal_voc.py
@@ -45,12 +45,18 @@ class pascal_voc(imdb):
         self._devkit_path = self._get_default_path() if devkit_path is None \
             else devkit_path
         self._data_path = os.path.join(self._devkit_path, 'VOC' + self._year)
+
+        '''
         self._classes = ('__background__',  # always index 0
                          'aeroplane', 'bicycle', 'bird', 'boat',
                          'bottle', 'bus', 'car', 'cat', 'chair',
                          'cow', 'diningtable', 'dog', 'horse',
                          'motorbike', 'person', 'pottedplant',
                          'sheep', 'sofa', 'train', 'tvmonitor')
+        '''
+
+        self._classes = ('__background__', 'truck','excavator', 'wheel', 'loader','bulldozer','dumper', 'person'
+,'car')
         self._class_to_ind = dict(zip(self.classes, xrange(self.num_classes)))
         self._image_ext = '.jpg'
         self._image_index = self._load_image_set_index()
